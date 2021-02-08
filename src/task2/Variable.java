97fac335d859167849265cbd696b69e9223c7722
bada13 <bada13@bk.ru>
2021-02-08 08:17:07 +0300

    task2,3 fix comment

--- src/task2/Variable.java
+++ src/task2/Variable.java
@@ -3,6 +3,6 @@ package task2;
 public class Variable {
     public static void main(String[] args) {
         double a = 6.66;
-        byte b = 6;
+        byte b = 127;
     }
 }
--- src/task3/Array.java
+++ src/task3/Array.java
@@ -3,20 +3,20 @@ package task3;
 public class Array {
     public static void main(String[] args) {
         int[] nums = {11, 22, 33, 44, 55};
-        System.out.print("Массив: " );
+        System.out.print("Массив: ");
         for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]+" " );
+            System.out.print(nums[i] + " ");
         }
         int tmp = nums[0];
-        nums[0] = nums[4];
-        nums[4] = tmp;
+        nums[0] = nums[nums.length-1];
+        nums[nums.length-1] = tmp;
         System.out.println();
-        System.out.print("Массив после обмена мест: " );
+        System.out.print("Массив после обмена мест: ");
         for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]+" ");
+            System.out.print(nums[i] + " ");
         }
         System.out.println();
-        int sum = nums[0] + nums[2];
-        System.out.println("Сумма первого и третьего элемента = "+sum);
+        int sum = nums[0] + nums[nums.length-3];
+        System.out.println("Сумма первого и третьего элемента = " + sum);
     }
 }
