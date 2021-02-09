3d3493daf045864c886f36805212e26c4a4dd060
bada13 <bada13@bk.ru>
2021-02-09 19:59:25 +0300

    task7 string

--- src/task7/cycle1.java
+++ src/task7/cycle1.java
@@ -3,27 +3,35 @@ import java.util.Scanner; // импорт сканнера
 
 public class cycle1 {
     public static void main(String[] args) {
-        int x,y,z; //константы x, y, z
-        x = 1;
-        y = 2;
-        z = 3;
+        String q,w,e; //константы q,w,e
+        q = "qq";
+        w = "ww";
+        e = "ee";
 
         Scanner input = new Scanner(System.in);
         System.out.print("Введите кол-во элементов в массиве: ");
         int size = input.nextInt();
-        int[] array = new int[size];
-        for (int i = 0; i < size; i++) {
+
+        input.nextLine();
+        String[] array2 = new String[size];
+        for (int i = 0; i < array2.length; i++) {
             System.out.print("Введите " + (i+1) + " элемент массива :");
-            array[i] = input.nextInt();
+            array2[i] = input.nextLine();
         }
 
-        int i = 0;
-        do {
-            if (array[i] == x || array[i] == y || array[i] == z){
-                System.out.print ("Данное значение имеется в константах");
+        for (String s : array2){
+            if (s.equals(q)) {
+                System.out.print("Данное значение имеется в константах");
                 break;
             }
-            i++;
-        } while (i < size);
+            if (s.equals(w)) {
+                System.out.print("Данное значение имеется в константах");
+                break;
+            }
+            if (s.equals(e)) {
+                System.out.print("Данное значение имеется в константах");
+                break;
+            }
+        }
     }
 }
