396b444eef86e1b44b73346dd0ffc26e66315716
bada13 <bada13@bk.ru>
2021-02-16 13:37:29 +0300

    task13 fix3

--- src/task13/data3.java
+++ src/task13/data3.java
@@ -6,21 +6,16 @@ public class data3 {
         Scanner input = new Scanner(System.in);
         System.out.print("Ввести строку: ");
         String text = input.nextLine();
+        String[] words = text.split("\\s+");
 
-        String str1;
-        str1 = text.replaceAll("[а-яА-Я\\d]", "").replaceAll("[-+*/,.?!]", "");
-        System.out.println("Слова состоящие только из латиницы: " + str1);
-
-        int length=str1.split("\\s+").length;
-        int count = 1;
-        for (int i=0;i<=str1.length()-1;i++)
-        {
-            if (str1.charAt(i) == ' ' && str1.charAt(i+1)!=' ')
-            {
+        System.out.println("Слова состоящие только из латиницы:");
+        int count = 0;
+        for(int j=0; j< words.length;j++){
+            if (words[j].matches("[a-zA-Z]+")){
+                System.out.println(words[j]);
                 count++;
             }
         }
-        System.out.println("Слова: " + count);
-        System.out.println("Слова: " + length);
+        System.out.println("Количество этих слов: " + count);
     }
 }
